8f4e38f39b15f8536205c3763ed275522818522a
Diego Mendez Rodriguez <dmendezrod.12@gmail.com>
2016-07-10 15:21:25 -0600

    Proyecto2

---
+++ Proyecto2/Proyecto/js/efecto2.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+     $("#seccion1").hover(function(){
+        $("#subSeccion1").show();
+         }, function(){
+                $("#subSeccion1").hide();
+            });
+});
+
+$(document).ready(function(){
+     $("#seccion2").hover(function(){
+        $("#subSeccion2").show();
+         }, function(){
+                $("#subSeccion2").hide();
+            });
+});
+
+$(document).ready(function(){
+     $("#seccion3").hover(function(){
+        $("#subSeccion3").show();
+         }, function(){
+                $("#subSeccion3").hide();
+            });
+});
+
+$(document).ready(function(){
+     $("#seccion4").hover(function(){
+        $("#subSeccion4").show();
+         }, function(){
+                $("#subSeccion4").hide();
+            });
+});
---
+++ Proyecto2/js/efectos.js
@@ -0,0 +1,5 @@
+$("#capa").hover(function(){
+    $("#mensaje").show();
+}, function(){
+    $("#mensaje").hide();
+});
