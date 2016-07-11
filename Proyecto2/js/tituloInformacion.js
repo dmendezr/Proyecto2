2cdb5f75f033eec4042c0e99d79143eff20f4f52
Diego Mendez Rodriguez <dmendezrod.12@gmail.com>
2016-07-11 11:47:54 -0600

    Cambio de Estilos en Informacion
    Cambio de Estilos

--- Proyecto2/js/tituloInformacion.js
+++ Proyecto2/js/tituloInformacion.js
@@ -1,22 +1,143 @@
-/*
+
 $(document).ready(function(){
-		$("#contenido-informacion").hover(
+		$("#txtNombre").hover(
 			function(){
-				$("#encabezado-informacion").css({"background-color":"#8904B1"});
+				$("#lblNombre").css({"transform": "scale(0.8)"});
+				$("#lblNombre").css({"color": "#8904B1"});
 			},function(){
-				$("#encabezado-informacion").css({"background-color":"white"});
+				$("#lblNombre").css({"transform": "inherit"});
+				$("#lblNombre").css({"color": "inherit"});
 			}
 		);
 });
-*/
+
+
 $(document).ready(function(){
-		$(".contenido-informacion").hover(
+		$("#txtCedula").hover(
 			function(){
-				$(".encabezado-informacion").css({"transform": "scale(0.8)"});
-				$(".encabezado-informacion").css({"color": "#8904B1"});
+				$("#lblCedula").css({"transform": "scale(0.8)"});
+				$("#lblCedula").css({"color": "#8904B1"});
 			},function(){
-				$(".encabezado-informacion").css({"transform": "inherit"});
-				$(".encabezado-informacion").css({"color": "inherit"});
+				$("#lblCedula").css({"transform": "inherit"});
+				$("#lblCedula").css({"color": "inherit"});
 			}
 		);
 });
+
+
+$(document).ready(function(){
+		$("#txtCP").hover(
+			function(){
+				$("#lblCP").css({"transform": "scale(0.8)"});
+				$("#lblCP").css({"color": "#8904B1"});
+			},function(){
+				$("#lblCP").css({"transform": "inherit"});
+				$("#lblCP").css({"color": "inherit"});
+			}
+		);
+});
+
+$(document).ready(function(){
+		$("#txtFN").hover(
+			function(){
+				$("#lblFN").css({"transform": "scale(0.8)"});
+				$("#lblFN").css({"color": "#8904B1"});
+			},function(){
+				$("#lblFN").css({"transform": "inherit"});
+				$("#lblFN").css({"color": "inherit"});
+			}
+		);
+});
+
+$(document).ready(function(){
+		$("#txtSexo").hover(
+			function(){
+				$("#lblSexo").css({"transform": "scale(0.8)"});
+				$("#lblSexo").css({"color": "#8904B1"});
+			},function(){
+				$("#lblSexo").css({"transform": "inherit"});
+				$("#lblSexo").css({"color": "inherit"});
+			}
+		);
+});
+
+
+$(document).ready(function(){
+		$("#txtEdad").hover(
+			function(){
+				$("#lblEdad").css({"transform": "scale(0.8)"});
+				$("#lblEdad").css({"color": "#8904B1"});
+			},function(){
+				$("#lblEdad").css({"transform": "inherit"});
+				$("#lblEdad").css({"color": "inherit"});
+			}
+		);
+});
+
+
+$(document).ready(function(){
+		$("#txtPais").hover(
+			function(){
+				$("#lblPais").css({"transform": "scale(0.8)"});
+				$("#lblPais").css({"color": "#8904B1"});
+			},function(){
+				$("#lblPais").css({"transform": "inherit"});
+				$("#lblPais").css({"color": "inherit"});
+			}
+		);
+});
+
+
+$(document).ready(function(){
+		$("#txtProvincia").hover(
+			function(){
+				$("#lblProvincia").css({"transform": "scale(0.8)"});
+				$("#lblProvincia").css({"color": "#8904B1"});
+			},function(){
+				$("#lblProvincia").css({"transform": "inherit"});
+				$("#lblProvincia").css({"color": "inherit"});
+			}
+		);
+});
+
+
+$(document).ready(function(){
+		$("#txtCanton").hover(
+			function(){
+				$("#lblCanton").css({"transform": "scale(0.8)"});
+				$("#lblCanton").css({"color": "#8904B1"});
+			},function(){
+				$("#lblCanton").css({"transform": "inherit"});
+				$("#lblCanton").css({"color": "inherit"});
+			}
+		);
+});
+
+$(document).ready(function(){
+		$("#txtDistrito").hover(
+			function(){
+				$("#lblDistrito").css({"transform": "scale(0.8)"});
+				$("#lblDistrito").css({"color": "#8904B1"});
+			},function(){
+				$("#lblDistrito").css({"transform": "inherit"});
+				$("#lblDistrito").css({"color": "inherit"});
+			}
+		);
+});
+
+$(document).ready(function(){
+		$("#txtDE").hover(
+			function(){
+				$("#lblDE").css({"transform": "scale(0.8)"});
+				$("#lblDE").css({"color": "#8904B1"});
+			},function(){
+				$("#lblDE").css({"transform": "inherit"});
+				$("#lblDE").css({"color": "inherit"});
+			}
+		);
+});
+
+
+
+
+
