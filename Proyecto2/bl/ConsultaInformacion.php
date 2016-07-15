bb6428acb526bd590fe6b17a462ebf4bdaf3256b
Diego Mendez Rodriguez <dmendezrod.12@gmail.com>
2016-07-15 16:40:40 -0600

    Prueba Consultas
    .

--- Proyecto2/bl/ConsultaInformacion.php
+++ Proyecto2/bl/ConsultaInformacion.php
@@ -4,10 +4,9 @@ include 'php/conectar.php';
 include 'php/obtener.php';
 
 function ConsultaNombre($cedula){
-		if(isset($_GET['cedula'])) {
-    	$id = $_GET['cedula'];
-		}
 		$persona = new Persona();
-		$resultado = $persona->getPersona($cedula);	
+		$resultado = $persona->getPersona($cedula);
+		return $resultado['Nombre'];
 }
 ?>
+	
