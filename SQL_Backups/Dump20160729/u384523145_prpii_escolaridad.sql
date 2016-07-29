78b648a398b84eb6833f68e8c28e017659eee560
Diego Mendez Rodriguez <dmendezrod.12@gmail.com>
2016-07-29 17:23:12 -0600

    Backup de Base de datos

--- SQL_Backups/Dump20160729/u384523145_prpii_escolaridad.sql
+++ SQL_Backups/Dump20160729/u384523145_prpii_escolaridad.sql
@@ -1,11 +1,8 @@
-﻿create database `u384523145_prpii`;
-
-USE `u384523145_prpii`;
 -- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
 --
--- Host: localhost    Database: proyectopr3
+-- Host: localhost    Database: u384523145_prpii
 -- ------------------------------------------------------
--- Server version	5.5.24-log
+-- Server version	5.7.14-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -17,36 +14,6 @@ USE `u384523145_prpii`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `direccion`
---
-
-DROP TABLE IF EXISTS `direccion`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `direccion` (
-  `ID` int(11) NOT NULL,
-  `Cedula` int(11) NOT NULL,
-  `Pais` varchar(45) DEFAULT NULL,
-  `Provincia` varchar(45) DEFAULT NULL,
-  `Canton` varchar(45) DEFAULT NULL,
-  `Distrito` varchar(45) DEFAULT NULL,
-  `DireccionExacta` varchar(45) NOT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `cedula_idx` (`Cedula`),
-  CONSTRAINT `direccion-persona` FOREIGN KEY (`Cedula`) REFERENCES `persona` (`Cedula`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `direccion`
---
-
-LOCK TABLES `direccion` WRITE;
-/*!40000 ALTER TABLE `direccion` DISABLE KEYS */;
-/*!40000 ALTER TABLE `direccion` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `escolaridad`
@@ -77,35 +44,6 @@ LOCK TABLES `escolaridad` WRITE;
 /*!40000 ALTER TABLE `escolaridad` DISABLE KEYS */;
 /*!40000 ALTER TABLE `escolaridad` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `persona`
---
-
-DROP TABLE IF EXISTS `persona`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `persona` (
-  `Cedula` int(11) NOT NULL,
-  `Nombre` varchar(45) NOT NULL,
-  `Apellido1` varchar(45) DEFAULT NULL,
-  `Apellido2` varchar(45) DEFAULT NULL,
-  `Sexo` varchar(45) DEFAULT NULL,
-  `FechaNacimiento` datetime DEFAULT NULL,
-  `Edad` int(11) DEFAULT NULL,
-  PRIMARY KEY (`Cedula`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `persona`
---
-
-LOCK TABLES `persona` WRITE;
-/*!40000 ALTER TABLE `persona` DISABLE KEYS */;
-INSERT INTO `persona` VALUES (1,'Diego','Mendez','Rodriguez','M','0000-00-00 00:00:00',20),(2,'Karen','Fonseca','Mendez','F','0000-00-00 00:00:00',21),(3,'Daniela','Marin','Flore','F','0000-00-00 00:00:00',22);
-/*!40000 ALTER TABLE `persona` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -116,4 +54,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-07-14 11:21:10
+-- Dump completed on 2016-07-29 17:18:43
