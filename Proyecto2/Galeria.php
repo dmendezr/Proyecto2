a8b0579965f4c404476959ec82e492cf019b394b
Maria09 <ma24fer@hotmail.com>
2016-08-03 11:26:44 -0600

    Archivo Glaeria

---
+++ Proyecto2/Galeria.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Galeria-Index</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<meta charset="utf-8">
+	<script type="text/javascript" src="js/jquery-3.0.0.min.js"></script>
+	<script type="text/javascript" src="js/efecto2.js"></script>
+	<script type="text/javascript" src="js/modernizr.custom.28468.js"></script>
+	<script type="text/javascript" src="js/tituloInformacion.js"></script>
+	<script type="text/javascript" src="js/efecto3.js"></script>
+	<noscript>
+			<link rel="stylesheet" type="text/css" href="css/nojs.css" />
+	</noscript>
+</head>
+<body>
+	<div class="wrapper">
+		<header>
+			<div class="logo">
+				<a href="#"><img src="images/logo.png"></a>
+		<nav>
+        	<ul id="menu">
+          		<li><a href="index.php">Inicio</a></li>
+          		<li><a href="information.php?id=1">Informacion</a>
+            		<ul>
+              			<li><a href="pasatiempos.php">Pasatiempos</a></li>
+              			<li><a href="escolaridad.php?id=1">Escolaridad</a></li>
+              			<li><a href="logros.php?id=1">Logros</a></li>
+            		</ul>  
+          		</li>
+          		<li><a href="">Galeria</a></li>
+          		<li><a href="aboutus.php">Sobre Nosotros</a></li>
+        	</ul>
+      </nav>
+		</header>
