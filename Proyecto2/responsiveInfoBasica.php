11da88c64e45bdb66bbc9ab4783d7bd0381a3427
Maria09 <ma24fer@hotmail.com>
2016-08-10 09:25:50 -0600

    responsive galeria

---
+++ Proyecto2/responsiveInfoBasica.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Portafolio-Index</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<meta charset="utf-8">
+	<script type="text/javascript" src="js/jquery-3.0.0.min.js"></script>
+	<script type="text/javascript" src="js/efecto2.js"></script>
+	<script type="text/javascript" src="js/modernizr.custom.28468.js"></script>
+	<script type="text/javascript" src="js/tituloInformacion.js"></script>
+	<script type="text/javascript" src="js/efecto3.js"></script>
+	<noscript>
+			<link rel="stylesheet" type="text/css" href="css/nojs.css" />
+	</noscript>
+</head>
+<body>
+	<div class="wrapper">
+		<header>
+			<div class="logo">
+				<a href="#"><img src="images/logo.png"></a>
+		
+		</header>
+
+		<div id="cambios">
+			
+		</div>
+
+</div>
+
+<?php  Include 'footer.php'?>
